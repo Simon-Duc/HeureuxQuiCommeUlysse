5cfdf2bfe2337d52648456946dc6d24e7b2fd035
Simon Duc <ducsimon@gmail.com>
2022-07-11 11:26:25 +0200

    add the 4 input tags left

--- front/ulysse/src/SignUp.jsx
+++ front/ulysse/src/SignUp.jsx
@@ -3,22 +3,40 @@ import React, {Component} from 'react';
 class SignUp extends Component {
   constructor() {
     super();
-    this.updateEmailField = this.updateEmailField.bind(this);
+    this.updateField = this.updateField.bind(this);
     this.state = {
-      email: ''
+      email: "mon@email.com",
+      password: "monPassw0rd",
+      name: "James",
+      lastname: "Bond"
     };
   }
 
-  updateEmailField(event){
-    this.setState({email: event.target.value});
+  updateField(event){
+    this.setState({
+      email: event.target.value,
+      password: event.target.value,
+      name: event.target.value,
+      lastname: event.target.value
+    });
   }
 
   render() {
     return(
-    <>
-      <h1>{this.state.email}</h1>
-      <input onChange={this.updateEmailField} type="email" name="email" />
-    </>);
+    <div className="form">
+      <h1>{JSON.stringify(this.state)}</h1>
+      <label htmlFor="email">Email:</label>
+      <input className="input" onChange={this.updateField} id="email" type="email" name="email" />
+      <label htmlFor="password">Password:</label>
+      <input className="input" onChange={this.updateField} id="password" type="password" name="password" />
+      <label htmlFor="passwordbis">Password bis:</label>
+      <input className="input" onChange={this.updateField} id="passwordbis" type="password" name="passwordbis" />
+      <label htmlFor="name">Name:</label>
+      <input className="input" onChange={this.updateField} id="name" type="test" name="name" />
+      <label htmlFor="lastname">Lastname:</label>
+      <input className="input" onChange={this.updateField} id="lastname" type="text" name="lastname" />
+      <input type="submit" value="Soumettre" />
+    </div>);
   }
 }
 
