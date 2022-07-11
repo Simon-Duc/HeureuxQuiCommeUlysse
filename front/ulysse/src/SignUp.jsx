36c881b088a43c2066bc121369c11a983c45aa8f
Simon Duc <ducsimon@gmail.com>
2022-07-11 10:58:06 +0200

    add the updateEmailField method and the onChange event to change the h1 title thanks to the input state value

--- front/ulysse/src/SignUp.jsx
+++ front/ulysse/src/SignUp.jsx
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
 
 class SignUp extends Component {
+  constructor() {
+    super();
+    this.updateEmailField = this.updateEmailField.bind(this);
+    this.state = {
+      email: ''
+    };
+  }
+
+  updateEmailField(event){
+    this.setState({email: event.target.value});
+  }
+
   render() {
-    return(<input type="email" name="email" />);
+    return(
+    <>
+      <h1>{this.state.email}</h1>
+      <input onChange={this.updateEmailField} type="email" name="email" />
+    </>);
   }
 }
 
